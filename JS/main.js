11981607a19ba50ef44002681617764af980fdfc
Marcel <marcel.majewski.marcel.majewski@gmail.com>
2023-11-18 22:02:17 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// 1
+
+
+// let randomNumber = Math.floor(Math.random() * 8)
+// randomNumber = 5
+
+// switch (randomNumber) {
+// 	case 0:
+// 		console.log(`that's 0`)
+// 		break
+// 	case 1:
+// 		console.log('It is certain')
+// 		break
+// 	case 2:
+// 		console.log('It is decidedly so')
+// 		break
+// 	case 3:
+// 		console.log('Reply hazy try again')
+// 		break
+// 	case 4:
+// 		console.log('Cannot predict now')
+// 		break
+// 	case 5:
+// 		console.log('Do not count on it')
+// 		break
+// 	case 6:
+// 		console.log('It is certain')
+// 		break
+// 	case 7:
+// 		console.log('It is certain')
+// 		break
+// 	case 8:
+// 		console.log('Signs point to yes')
+// 		break
+// 	default:
+// 		console.log(`That's not the number within the range 0-8`)
+// }
+
+// 2
+
+// let runTime = 35;
+// let runDistance = 3.5;
+
+// if (runTime <= 30 && runDistance > 3.5) {
+//   console.log("You're super fast!");
+// } else if (runTime >= 30 && runDistance <= 3) {
+//   console.log("You're not making your pace!");
+// } else if (runTime > 30 || runDistance > 3) {
+//   console.log("Nice workout!");
+// } else {
+//   console.log("Keep on running!");
+// }
